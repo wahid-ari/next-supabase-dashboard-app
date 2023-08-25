d3d133ffd12f5d23ee34cf5b497ad766e768d7ae
wahidari <wahiid.ari@gmail.com>
2023-08-25 13:59:12 +0700

    create sitemap.xml

---
+++ app/api/sitemap/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+import { supabase } from '@/libs/supabase';
+
+export async function GET() {
+  const { data: books } = await supabase.from('book_books').select(`slug`).order('id');
+  const { data: authors } = await supabase.from('book_authors').select(`slug`).order('id');
+  const { data: genres } = await supabase.from('book_genres').select(`slug`).order('id');
+  return NextResponse.json({ books, authors, genres }, { status: 200 });
+}
