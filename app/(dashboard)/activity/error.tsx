e966370b5b01f64d33a2944fea583af7a00710bd
wahidari <wahiid.ari@gmail.com>
2023-08-30 16:09:49 +0700

    update error page using ErrorPage component

--- app/(dashboard)/activity/error.tsx
+++ app/(dashboard)/activity/error.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import ErrorPage from '@/components/layout/ErrorPage';
+
 // Error components must be Client Components
 // Try to make error when fetching data in activuty page to activate this error
 // import { useEffect } from 'react';
@@ -10,22 +12,5 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
   //   console.error(error);
   // }, [error]);
 
-  return (
-    <div className='h-[500px] flex items-center justify-center'>
-      <div className='text-center'>
-        <h2 className='text-5xl font-semibold mb-4'>500</h2>
-        <p className='text-lg font-medium'>Server Error Activity Page</p>
-        <p className='text-base mb-4'>Message : {error.message}</p>
-        <button
-          onClick={
-            // Attempt to recover by trying to re-render the segment
-            () => reset()
-          }
-          className='text-white bg-sky-600 transition-all hover:bg-sky-500 rounded px-2 py-1 text-sm'
-        >
-          TRY AGAIN
-        </button>
-      </div>
-    </div>
-  );
+  return <ErrorPage status='500' title='Server Error Activity Page' message={error.message} reset={reset} />;
 }
