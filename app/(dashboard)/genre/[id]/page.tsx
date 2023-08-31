72a5a9872b0599d1f8685ee2acdb223b705bcf6a
wahidari <wahiid.ari@gmail.com>
2023-08-31 22:06:49 +0700

    update Detail Genre Dashboard page

---
+++ app/(dashboard)/genre/[id]/page.tsx
@@ -0,0 +1,43 @@
+import type { Metadata, ResolvingMetadata } from 'next';
+
+import { siteConfig } from '@/config/site';
+
+async function getData(id: string) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_ROUTE}/api/genre?id=${id}`);
+  if (!res.ok) {
+    // This will activate the closest `error.tsx` Error Boundary
+    throw new Error('Failed to fetch genre data');
+  }
+  return res.json();
+}
+
+export async function generateMetadata({ params }, parent: ResolvingMetadata): Promise<Metadata> {
+  const data = await getData(params.id);
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+  return {
+    title: `${data.name}`,
+    description: `Discover ${data.name} Books`,
+    alternates: {
+      canonical: `${siteConfig.url}/genre/${params.id}`,
+    },
+    openGraph: {
+      title: `${data.name}`,
+      description: `Discover ${data.name} Books`,
+      url: `${siteConfig.url}/genre/${params.id}`,
+      images: [`${siteConfig.url}/api/og?title=${data.name} Books `, ...previousImages],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${data.name}`,
+      description: `Discover ${data.name} Books`,
+      images: [`${siteConfig.url}/api/og?title=${data.name} Books `, ...previousImages],
+    },
+  };
+}
+
+export default async function Page({ params }) {
+  // const data = await getData(params.id);
+  // console.log(data, '\n');
+  return '';
+}
