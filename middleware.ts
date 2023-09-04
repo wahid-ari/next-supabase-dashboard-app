7ecddbe39c5af45256ff781ed153541079fea86a
wahidari <wahiid.ari@gmail.com>
2023-09-04 22:02:02 +0700

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,21 @@
-export { default } from 'next-auth/middleware';
+import { NextRequest, NextResponse } from 'next/server';
+import { withAuth } from 'next-auth/middleware';
+
 // FIX this https://codevoweb.com/setup-and-use-nextauth-in-nextjs-13-app-directory/
 // FIX this
-export const config = { matcher: ['/dashboard'] };
+// https://github.com/nextauthjs/next-auth/issues/5695
+
+export function middleware(request: NextRequest) {
+  // check cookie, if valid return early
+  // console.log(request);
+  // console.log(request.nextUrl.pathname);
+  // console.log(request.nextUrl.pathname.startsWith('/old'));
+  if (request.nextUrl.pathname.startsWith('/old')) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+  return withAuth(request as any);
+}
+
+// To include all dashboard nested routes (sub pages like /dashboard/settings, /dashboard/profile)
+// you can pass matcher: "/dashboard/:path*" to config.
+export const config = { matcher: ['/old', '/dashboard/:path*', '/genre/:path*'] };
