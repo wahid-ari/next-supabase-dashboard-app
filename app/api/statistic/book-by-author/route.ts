3504aa661b3dafb51382e46b65e0d5d1afde8a30
wahidari <wahiid.ari@gmail.com>
2023-09-05 19:59:25 +0700

    create Statistic API endpoint

---
+++ app/api/statistic/book-by-author/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+import { supabase } from '@/libs/supabase';
+
+export async function GET(request: Request) {
+  const { data: books_authors } = await supabase.from('book_authors').select(`*, book_books (*)`).order('id');
+  // Make an array of object structure
+  let items = [];
+  for (const author of books_authors) {
+    items.push({
+      id: author.id,
+      label: author.name,
+      total: author.book_books.length,
+    });
+  }
+  let sortedData = items.sort((a: any, b: any) => b.total - a.total).slice(0, 10);
+  return NextResponse.json(sortedData, { status: 200 });
+}
---
+++ app/api/statistic/book-by-genre/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server';
+
+import { supabase } from '@/libs/supabase';
+
+export async function GET(request: Request) {
+  const { data: books_genres } = await supabase.from('book_books_genres').select(`*`).order('id');
+  const { data: genres } = await supabase.from('book_genres').select(`*`).order('id');
+  // Make an array of object structure
+  let items = [];
+  for (const genre of genres) {
+    items.push({
+      id: genre.id,
+      label: genre.name,
+      total: 0,
+    });
+  }
+  // Count total book that have same genre
+  let result = [];
+  for (const item of items) {
+    for (const book_genre of books_genres) {
+      if (book_genre.genre_id == item.id) {
+        let filtered = items.filter((i) => i.id == book_genre.genre_id)[0];
+        filtered.total += 1;
+        result.push(filtered);
+      }
+    }
+  }
+  // Remove duplicate values from an array of objects in javascript
+  // https://stackoverflow.com/questions/45439961/remove-duplicate-values-from-an-array-of-objects-in-javascript
+  let data = result.reduce((unique, o) => {
+    if (!unique.some((obj: any) => obj.id === o.id)) {
+      unique.push(o);
+    }
+    return unique;
+  }, []);
+  let sortedData = data.sort((a: any, b: any) => b.total - a.total).slice(0, 10);
+  return NextResponse.json(sortedData, { status: 200 });
+}
