6a45ddaa4e1dd9ed043184d3e7cd63916a8e256a
wahidari <wahiid.ari@gmail.com>
2023-09-07 11:56:54 +0700

    set Log API endpoint to dynamic

--- app/api/log/route.ts
+++ app/api/log/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 
 import { supabase } from '@/libs/supabase';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const { data } = await supabase.from('book_logs').select(`*, book_users (username, name, type)`).order('id');
   return NextResponse.json(data, { status: 200 });
