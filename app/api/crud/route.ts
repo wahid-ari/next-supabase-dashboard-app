e2b77e325b8633fc84ff27d940e314a05012121c
wahidari <wahiid.ari@gmail.com>
2023-09-08 13:41:12 +0700

    update example API endpoint CORS

--- app/api/crud/route.ts
+++ app/api/crud/route.ts
@@ -3,7 +3,21 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
-  return NextResponse.json({ message: 'Success GET' }, { status: 200 });
+  // TODO Docs https://nextjs.org/docs/app/building-your-application/routing/route-handlers#cors
+  // TODO Docs https://github.com/vercel/next.js/discussions/47933
+  return NextResponse.json(
+    { message: 'Success GET' },
+    {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers':
+          'Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version',
+      },
+    },
+  );
 }
 
 export async function POST(request: Request) {
