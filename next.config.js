dc2e57a22203d13b3af0c7659597f5e85d34df13
wahidari <wahiid.ari@gmail.com>
2024-01-17 20:58:42 +0700

    feat: Using CORS in Next.js to handle cross-origin requests

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // TODO Docs https://vercel.com/guides/how-to-enable-cors#enabling-cors-in-a-next.js-app
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     // https://nextjs.org/docs/pages/api-reference/components/image#domains
     // domains: ['images-na.ssl-images-amazon.com', 'i.gr-assets.com', 'images.unsplash.com'],
